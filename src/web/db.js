ee0b0f2f72471de86e596736777d0b880cf2f71b
Cap0703 <c.oldoerp26@bosco.org>
2026-01-22 00:02:45 -0800

    Some more table initialization and tests.

--- src/web/db.js
+++ src/web/db.js
@@ -10,6 +10,12 @@ export const pool = new Pool({
 });
 
 export async function initializeDatabase() {
+    await initUsers();
+    await initLogs();
+    await initScanners();
+}
+
+async function initUsers() {
   const client = await pool.connect();
     try {
         await client.query(`
@@ -24,6 +30,57 @@ export async function initializeDatabase() {
     } catch (err) {
         console.error('Error initializing web user table:', err);
     } finally {
+        client.release();
+    }
+}
+
+async function initLogs() {
+  const client = await pool.connect();
+    try {
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS logs (
+                id SERIAL PRIMARY KEY,
+                period TEXT,
+                scanner_location TEXT,
+                scanner_id TEXT,
+                student_id TEXT,
+                class_late TEXT,
+                first_name TEXT,
+                last_name TEXT,
+                time_scanned TEXT,
+                date_scanned TEXT,
+                marked TEXT
+            );
+        `);
+        console.log('Logs table initialized.');
+    }
+    catch (err) {
+        console.error('Error initializing logs table:', err);
+    }
+    finally {
+        client.release();
+    }
+}
+
+async function initScanners() {
+    const client = await pool.connect();
+    try {
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS scanners (
+                id SERIAL PRIMARY KEY,
+                scanner_id TEXT UNIQUE,
+                scanner_location TEXT,
+                scanner_status TEXT,
+                last_sync TEXT,
+                battery_level TEXT
+            );
+            `);
+        console.log('Scanners table initialized.');
+    }
+    catch (err) {
+        console.error('Error initializing scanners table:', err);
+    }
+    finally {
         client.release();
     }
 }
--- src/web/server.js
+++ src/web/server.js
@@ -1,8 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import { initializeDatabase } from './db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 await initializeDatabase();
 
